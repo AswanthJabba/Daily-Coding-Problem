4bcc6708e3cdbcf89fffa8cd71373e98c9aeecef
AswanthJabba <86704864+AswanthJabba@users.noreply.github.com>
2021-10-18 13:03:47 +0530

    Update Problem #2 [Hard].py

--- Solutions/Problem #2 [Hard].py
+++ Solutions/Problem #2 [Hard].py
@@ -7,3 +7,20 @@ For example, if our input was [1, 2, 3, 4, 5], the expected output would be [120
 
 Follow-up: what if you can't use division?
 '''
+def arrproduct(arr):
+    product=[0]*len(arr)
+    l=[0]*len(arr)
+    r=[0]*len(arr)
+    l[0]=1
+    r[len(arr)-1]=1
+    for i in range(1,len(arr)):
+        l[i]=arr[i-1]*l[i-1]
+    for i in range(len(arr)-2,-1,-1):
+        r[i]=arr[i+1]*r[i+1]
+    for i in range(len(arr)):
+        product[i]=l[i]*r[i]
+    return product
+    
+if __name__=='__main__':
+    arr=[1, 2, 3, 4, 5]
+    print(arrproduct(arr))
